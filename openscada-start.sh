654fdb261161878a8aca6a88d9a8e42a04f6c9a1
Jaran Keowma <jaran_k@hotmail.com>
2020-12-11 10:39:19 +0700

    Add files via upload

---
+++ openscada-start.sh
@@ -0,0 +1,1 @@
+docker run -itd -p 10002:10002 -p 10005:10005 --device /dev/ttyUSB0:/dev/ttyUSB0 --name openscada -v /root/oscada-root/:/root/.openscada oscada-arm64v8:bionic /usr/bin/openscada_AGLKS --noX11
